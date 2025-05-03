ae91f23b97e16bcb1ffca1a32f59525bfc37b8bd
serenidpityy <1343608717@qq.com>
2025-05-03 15:13:14 +0800

    编写了用户登录时用户名不正确以及密码不正确的异常处理功能

---
+++ src/main/java/com/wfit/springbootshop/service/ex/WrongPasswordException.java
@@ -0,0 +1,24 @@
+package com.wfit.springbootshop.service.ex;
+
+
+public class WrongPasswordException extends ServiceException {
+    public WrongPasswordException() {
+        super();
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+
+    public WrongPasswordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WrongPasswordException(Throwable cause) {
+        super(cause);
+    }
+
+    protected WrongPasswordException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
